aa28c181f6b5390a2d7bd1ebba44017fd19e311a
Fembunny <bellacyrne@gmail.com>
2025-05-11 15:05:01 -0300

    create db.migration, v1, v2, v3

--- api/src/main/java/com/eventostec/api/domain/coupon/Coupon.java
+++ api/src/main/java/com/eventostec/api/domain/coupon/Coupon.java
@@ -25,7 +25,7 @@ public class Coupon {
 
     private String code;
     private Integer discount;
-    private Date validade;
+    private Date valid;
 
     @ManyToOne
     @JoinColumn(name = "event_id")
